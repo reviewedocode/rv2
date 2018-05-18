40a98b1dc5e99fc51c007bc951d46d5f153f65ae
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2018-05-18 21:47:24 +0100

    tries to add token

---
+++ web/src/components/SignupForm/index.js
@@ -0,0 +1,102 @@
+// @flow
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router';
+import { css, StyleSheet } from 'aphrodite';
+import Input from '../Input';
+
+const styles = StyleSheet.create({
+  card: {
+    maxWidth: '500px',
+    padding: '3rem 4rem',
+    margin: '2rem auto',
+  },
+});
+
+type Props = {
+  onSubmit: () => void,
+  submitting: boolean,
+  handleSubmit: () => void,
+}
+
+class SignupForm extends Component {
+  props: Props
+
+  handleSubmit = data => this.props.onSubmit(data);
+
+  render() {
+    const { handleSubmit, submitting } = this.props;
+
+    return (
+      <form
+        className={`card ${css(styles.card)}`}
+        onSubmit={handleSubmit(this.handleSubmit)}
+      >
+        <h3 style={{ marginBottom: '2rem', textAlign: 'center' }}>Create an account</h3>
+        <Field
+          name="first_name"
+          type="text"
+          component={Input}
+          placeholder="First name"
+          className="form-control"
+        />
+        <Field
+          name="last_name"
+          type="text"
+          component={Input}
+          placeholder="Last name"
+          className="form-control"
+        />
+        <Field
+          name="email"
+          type="email"
+          component={Input}
+          placeholder="Email"
+          className="form-control"
+        />
+        <Field
+          name="password"
+          type="password"
+          component={Input}
+          placeholder="Password"
+          className="form-control"
+        />
+        <button
+          type="submit"
+          disabled={submitting}
+          className="btn btn-block btn-primary"
+        >
+          {submitting ? 'Submitting...' : 'Sign up'}
+        </button>
+        <hr style={{ margin: '2rem 0' }} />
+        <Link to="/login" className="btn btn-block btn-secondary">
+          Login to your account
+        </Link>
+      </form>
+    );
+  }
+}
+
+const validate = (values) => {
+  const errors = {};
+  if (!values.first_name) {
+    errors.first_name = 'Required';
+  }
+  if (!values.last_name) {
+    errors.last_name = 'Required';
+  }
+  if (!values.email) {
+    errors.email = 'Required';
+  }
+  if (!values.password) {
+    errors.password = 'Required';
+  } else if (values.password.length < 6) {
+    errors.password = 'Minimum of 6 characters';
+  }
+  return errors;
+};
+
+export default reduxForm({
+  form: 'signup',
+  validate,
+})(SignupForm);
